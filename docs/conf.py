6fa2a954553a86b9bfe8a29ba55eb30f82e54295
K. Mertens <mertens@ihp-microelectronics.com>
2025-11-20 09:44:03 +0000

    docs/conf.py: Project information, add comments.

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ import os
 import re
 
 # -- Project information -----------------------------------------------------
+# Unfortunately, the Sphinx documentation builder automatically appends 'documentation':
 project = "IHP 130nm BiCMOS Open Source PDK Documentation"
+# Multiline copyright statements are not supported:
 copyright = "2024, IHP PDK Authors"
 author = "IHP PDK Authors"
 
